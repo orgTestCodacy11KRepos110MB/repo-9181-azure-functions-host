aa5d49371eb2f5cd533b6c44bc8a38caec5ca2da
Surgupta <surgupta@microsoft.com>
2022-08-17 14:29:52 -0700

    Updated ParseData method for RichBindingReferenceTye

--- src/WebJobs.Script/Binding/GeneralScriptBindingProvider.cs
+++ src/WebJobs.Script/Binding/GeneralScriptBindingProvider.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Reflection;
 using System.Runtime.Loader;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Script.Abstractions.Description.Binding;
 using Microsoft.Azure.WebJobs.Script.Description;
 using Microsoft.Azure.WebJobs.Script.Extensibility;
 using Microsoft.Extensions.Logging;
